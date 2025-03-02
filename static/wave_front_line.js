b3f64d9c0a1cc5947542cb3f7726f1d6529878be
Bastain Slater-Tyler <Bastiantyler32@gmail.com>
2025-03-01 20:25:10 -0800

    Update wave_front_line.js

--- static/wave_front_line.js
+++ static/wave_front_line.js
@@ -1,21 +1,30 @@
 let waveSpeed = 2;
 let disturbance;
-let numDisturbances = 5;
+let numDisturbances = 15;
 let lineX;
 let lineY1, lineY2;
 let spacing;
 let maxSpacing;
 let target = 0;
 let secondaryWave;
+let centerY;
+let theta = 45;
 
 let linesArray = [];
+let secondaryWaves = [];
+let disturbances = [];
 
 function setup(){
     createCanvas(400,400);
+    angleMode(DEGREES)
+    centerY = height / 3;
+    lineX = width / 5 - 50;
 
-    lineX = width / 5;
-    lineY1 = height / 2 - 100;
-    lineY2 = height / 2 + 100;
+    lineY1 = centerY - height / 2 - 100;
+    lineY2 = centerY + height / 2 + 100;
+
+    
+    
     maxSpacing = (lineY2 - lineY1) / (numDisturbances - 1);
     spacing = 0;
 
@@ -33,10 +42,15 @@ function drawDisturbances(value){
     for(let i = 0; i < numDisturbances; i++){
         let dotY = lineY1 + i * maxSpacing;
         disturbance = ellipse(lineX + (maxSpacing*value)/2, dotY, 10);
+        disturbances.push([lineX + (maxSpacing*value)/2, dotY, 10]);
 
         noFill();
         stroke(255,255,0);
         ellipse(lineX + (value*maxSpacing)/2, dotY, spacing); //here for spacing
+
+        if(spacing >= maxSpacing){
+            secondaryWaves.push([lineX + (value*maxSpacing)/2, dotY, spacing]);
+         }
         fill(255,0,0);
         noStroke();
     }
@@ -57,8 +71,32 @@ function drawWaveFronts(){
     }
 }
 
+function drawAllDisturbances(){
+
+    for(let i = 0; i < disturbances.length; i++){
+
+        noFill();
+        stroke(255,255,0);
+
+        let disturbanceData = disturbances[i];
+        ellipse(disturbanceData[0], disturbanceData[1], disturbanceData[2]);
+    }
+}
+
+function drawSecondaryWavefronts(){
+    noFill();  // Add this line
+    stroke(255,255,0);
+    for(let i = 0; i < secondaryWaves.length; i++){
+        let secondaryWavesData = secondaryWaves[i];
+        ellipse(secondaryWavesData[0], secondaryWavesData[1], secondaryWavesData[2]);
+    }
+}
+
 function draw(){
    background(30)
+    
+   translate(lineX, centerY);
+   rotate(theta)
 
     spacing = min(spacing + waveSpeed * 0.5, maxSpacing);
     
@@ -67,11 +105,13 @@ function draw(){
     drawNewWaveFront(target);
 
 
-     if(spacing >= maxSpacing){
+     if(spacing >= maxSpacing && target <= 7){
         target ++
         spacing = 0;
      }
 
      drawWaveFronts();
+     drawAllDisturbances();
+     drawSecondaryWavefronts();
     
 }
