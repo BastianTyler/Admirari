b6403f664cb2ad8f0594584c4ccc0252677dc519
BastianTyler <66173323+BastianTyler@users.noreply.github.com>
2025-03-07 10:17:58 -0800

    Added coding area

---
+++ routes/text_venture_routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+tv_bp = Blueprint('text_venture', __name__, url_prefix='/text_venture')
+
+@tv_bp.route('/tv_home')
+def tv_home():
+    return render_template('text_venture/tv_home.html')
+
+@tv_bp.route('/tv_about')
+def tv_about():
+    return render_template('text_venture/tv_about.html')
+
+@tv_bp.route('/tv_day_one')
+def tv_day_one():
+    return render_template('text_venture/tv_day_one.html')
