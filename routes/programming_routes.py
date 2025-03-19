8a7093b8e3a189f3063638c52bf0f095637b4dce
BastianTyler <66173323+BastianTyler@users.noreply.github.com>
2025-03-19 07:24:11 -0700

    Added godot note/project area
    Added some more links to base, I think I will update to make it so most content gets injected at route level to prevent conflicts.

--- routes/programming_routes.py
+++ routes/programming_routes.py
@@ -1,30 +1,59 @@
 from flask import Blueprint, render_template
 
 programming_bp = Blueprint('programming', __name__, url_prefix='/programming')
+godot_bp = Blueprint('godot', __name__, url_prefix='/programming/godot')
 
 
 # Define Links for sidebar
-SIDEBAR_TABLE_OF_CONTENTS = [
+JAVA_SIDEBAR_TABLE_OF_CONTENTS = [
     {"title": "Java Home", "url": "/programming/java/home_java"},
     {"title": "Linked Lists", "url": "/programming/java/linked_list"},
     {"title": "Lambda", "url": "/programming/java/java_lambda"},
 ]
 
+GODOT_SIDEBAR_TABLE_OF_CONTENTS = [
+    {"title": "Godot Home", "url": "/programming/godot/godot_home"},
+    {"title": "Notes", "url": "/programming/godot/godot_notes"}
+]
+
+@programming_bp.context_processor
+def inject_programming_context():
+    return {
+        'page_type': 'programming',
+        'table_of_contents': JAVA_SIDEBAR_TABLE_OF_CONTENTS
+    }
+
+@godot_bp.context_processor
+def inject_programming_context():
+    return {
+        'page_type': 'programming',
+        'table_of_contents': GODOT_SIDEBAR_TABLE_OF_CONTENTS
+    }
+
 
 @programming_bp.route('/java/home_java')
 def home_java():
-    return render_template('/programming/java/home_java.html', 
-                           table_of_contents=SIDEBAR_TABLE_OF_CONTENTS)
+    return render_template('/programming/java/home_java.html') 
+
 
 @programming_bp.route('/java/linked_list')
 def linked_list():
-    return render_template('/programming/java/linked_list.html', 
-                           table_of_contents=SIDEBAR_TABLE_OF_CONTENTS)
+    return render_template('/programming/java/linked_list.html') 
+
 
 @programming_bp.route('/java/java_lambda')
 def java_lambda():
-    return render_template('/programming/java/java_lambda.html', 
-                           table_of_contents=SIDEBAR_TABLE_OF_CONTENTS)
+    return render_template('/programming/java/java_lambda.html')
+
+@godot_bp.route('/godot_home')
+def godot_home():
+    return render_template('/programming/godot/godot_home.html')
+
+@godot_bp.route('/godot_notes')
+def godot_notes():
+    return render_template('/programming/godot/godot_notes.html') 
+
+
 
 
 
